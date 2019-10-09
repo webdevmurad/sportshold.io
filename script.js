91e21099829954cf480278f7ecfd7da165b073c9
Murad <murad-arslangiraev-1994@mail.ru>
2019-10-10 02:56:15 +0300

    модалки скрипты

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let tab = document.querySelector('.feedback-blocks'),
-    info = document.querySelectorAll('.feedback-block'),
-    tabContent = document.querySelectorAll('.feedback-text__feedback');
+
+
+
